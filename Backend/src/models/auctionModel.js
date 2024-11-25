660143b014c44e7bf7f570b4c40ee62ebc9d26fd
Piyush Choudhary <piyush.choudhary6264@gmail.com>
2024-11-25 17:51:10 +0530

    add auction and bid model

---
+++ Backend/src/models/auctionModel.js
@@ -0,0 +1,50 @@
+import mongoose from 'mongoose';
+
+
+
+const auctionSchema = new Schema({
+    product:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+        required: true
+    },
+
+    seller: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Seller',
+        required: true
+    },
+
+    startingPrice: {
+        type: Number,
+        required: true
+    },
+
+    currentBid: {
+        type: Number,
+        default: 0
+    },
+
+    bidEndDate: {
+        type: Date,
+        required: true
+    },
+
+    bids: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Bid'
+        }
+    ],
+
+    status: {
+        type: String,
+        enum: ['active', 'completed', 'cancelled'],
+        default: 'active'
+    },
+}, {timestamps: true}
+);
+
+const Auction = mongoose.model('Auction', auctionSchema);
+
+export default Auction;
---
+++ Backend/src/models/bidModel.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+
+
+const bidSchema = new Schema({
+    auction: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Auction',
+        required: true
+    },
+
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+
+    amount: {
+        type: Number,
+        required: true
+    },
+
+    bidTime: {
+        type: Date,
+        default: Date.now
+    }
+}, {timestamps: true}
+);
+
+const Bid = mongoose.model('Bid', bidSchema);
+
+export default Bid;
