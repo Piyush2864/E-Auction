46dfab79ce42b106246c909ba5b44ee707ced66d
Piyush Choudhary <piyush.choudhary6264@gmail.com>
2024-11-26 10:51:48 +0530

    setup server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,41 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import connectToDb from '../Backend/src/db/config.js';
+import cookieParser from 'cookie-parser';
+import userRoute from '../Backend/src/routes/userRoutes.js';
+import sellerRoute from '../Backend/src/routes/sellerRoute.js';
+import productRoute from '../Backend/src/routes/productRoute.js';
+import categoryRoute from '../Backend/src/routes/categoryRoutes.js';
+import bidRoute from '../Backend/src/routes/bidRoutes.js';
+import auctonRoute from '../Backend/src/routes/auctionRoutes.js';
 
 
+
+dotenv.configDotenv();
 
 const app = express();
 
+app,use(cors());
 
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use('api/v1/auction/user', userRoute);
+app.use('api/v1/auction/seller', sellerRoute);
+app.use('api/v1/auction/product', productRoute);
+app.use('api/v1/auction/category', categoryRoute);
+app.use('api/v1/auction/bid', bidRoute);
+app.use('api/v1/auction/auction', auctonRoute);
+
+
+connectToDb();
+
+app.get('/', (req, res) => {
+    res.send('Hello world')
+});
+
+app.listen(process.env.PORT, ()=> {
+    console.log(`Server is running on port ${process.env.PORt}`);
+});
