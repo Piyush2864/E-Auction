fe5dbec16ec0b861714b9c583526d2de268c8ca3
Piyush Choudhary <piyush.choudhary6264@gmail.com>
2024-11-26 10:06:44 +0530

    Connect to mongodb database

---
+++ Backend/src/db/config.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/E-Auction';
+
+const connectToDb = ()=> {
+    mongoose.connect(mongoURI, {
+
+    }).then(()=> {
+        console.log("Successfully connected to mongodb.")
+    }).catch(err=> console.error('Error in connecting to mongodb.'))
+};
+
+export default connectToDb;
