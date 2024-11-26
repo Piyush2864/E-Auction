9d157b7b4a0bbb09a19650e8db97a2062652fd4a
Piyush Choudhary <piyush.choudhary6264@gmail.com>
2024-11-26 12:31:34 +0530

    write comments

--- Backend/server.js
+++ Backend/server.js
@@ -9,19 +9,19 @@ import categoryRoute from '../Backend/src/routes/categoryRoutes.js';
 import bidRoute from '../Backend/src/routes/bidRoutes.js';
 import auctonRoute from '../Backend/src/routes/auctionRoutes.js';
 
-
-
+// Load environment variables from the .env file
 dotenv.configDotenv();
 
+// Initialize the Express application
 const app = express();
 
-app,use(cors());
-
+// Middleware setup
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
-
+// Mount API routes
 app.use('api/v1/auction/user', userRoute);
 app.use('api/v1/auction/seller', sellerRoute);
 app.use('api/v1/auction/product', productRoute);
@@ -29,13 +29,15 @@ app.use('api/v1/auction/category', categoryRoute);
 app.use('api/v1/auction/bid', bidRoute);
 app.use('api/v1/auction/auction', auctonRoute);
 
-
+// Establish a connection to the database
 connectToDb();
 
+// Root route to test the server
 app.get('/', (req, res) => {
     res.send('Hello world')
 });
 
+// Start the server and listen on the specified port
 app.listen(process.env.PORT, ()=> {
     console.log(`Server is running on port ${process.env.PORt}`);
 });
