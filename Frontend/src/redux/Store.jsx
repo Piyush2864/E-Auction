93bc4e3b133884acfa2f55d2f8266771d8671192
Piyush Choudhary <piyush.choudhary6264@gmail.com>
2024-12-01 18:21:24 +0530

    adding reducers

--- Frontend/src/redux/Store.jsx
+++ Frontend/src/redux/Store.jsx
@@ -1,11 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authReducer from '../redux/Slices/AuthSlice';
+import userReducer from '../redux/Slices/UserSlice';
+import sellerReducer from '../redux/Slices/SellerSlice';
+import categoryReducer from '../redux/Slices/CategorySlice';
+import productReducer from  '../redux/Slices/ProductSlice';
+import bidReducer from '../redux/Slices/BidSlice';
 
 
 
 
 const store = configureStore({
     reducer: {
-      
+      auth: authReducer,
+      user: userReducer,
+      seller: sellerReducer,
+      category: categoryReducer,
+      product: productReducer,
+      bid: bidReducer
     }
 });
 
