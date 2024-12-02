31744347943601417ddc90453f989120a789174c
Piyush Choudhary <piyush.choudhary6264@gmail.com>
2024-12-02 12:04:27 +0530

    deleting some files

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,19 @@
-
-
 import './App.css'
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Login from './component/Login';
+import SignupPage from './component/Signup';
+
 
 function App() {
 
   return (
     <>
+    <Router>
+      <Routes>
+      <Route path='/signup' element={<SignupPage/>}/>
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </Router>
     </>
   )
 }
